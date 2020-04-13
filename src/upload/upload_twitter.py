8d7fc4ce4184d5de99ddcf2f92edab79e46498c3
apruniergold <alexis@goldbaum.io>
2020-04-13 15:47:56 +0200

    Finish upload twitter

--- src/upload/upload_twitter.py
+++ src/upload/upload_twitter.py
@@ -1,15 +1,29 @@
 import tweepy
+import datetime
+import pathlib
+import os
 
-consumer_key = "xxxxxxxxxxxxxxxx"
-consumer_secret = "xxxxxxxxxxxxxxxx"
-access_token = "xxxxxxxxxxxxxxxx"
-access_token_secret = "xxxxxxxxxxxxxxxx"
+consumer_key = "eMhpRZbngPMLg95L2JZnBIk2P"
+consumer_secret = "OrzR4HDMOzLNauCdRcT2dRCDA5eQkEDOPb7wxpoM11ZbakL513"
+access_token = "1247554748737257472-AC5JfRfNkgeqUMQtpChZ7cVTrKWjr2"
+access_token_secret = "JRArrhqeXoTIIjuH5ZY7Tt0s4QBB78rd8NeNiTQ1zmdKu"
+
+project_path = str(pathlib.Path(__file__).parent.parent.parent.absolute())
+today = datetime.date.today().strftime('%Y-%m-%d')
+today_fr = datetime.date.today().strftime('%d-%m-%Y')
+youtube_video_id_path = os.path.join(project_path, "data", today, "youtube_video_id.txt")
+
+if not os.path.exists(youtube_video_id_path):
+    print("Youtube video not found")
+    exit()
+
+f = open("demofile.txt", "r")
+youtube_video_id = f.read()
+f.close()
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
-# authentication of access token and secret
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# update the status
-api.update_status(status="Hello Everyone !")
+api.update_status(status=f"Hɪɢʜʟɪᴛᴇ™ du {today_fr} sur le COVID-19\n\nhttps://youtube.com/watch?v={youtube_video_id}")
--- src/upload/upload_youtube.py
+++ src/upload/upload_youtube.py
@@ -1,4 +1,3 @@
-import argparse
 import http.client
 import httplib2
 import os
@@ -8,49 +7,31 @@ import datetime
 import pathlib
 import json
 
-import google.oauth2.credentials
-import google_auth_oauthlib.flow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-# Explicitly tell the underlying HTTP transport library not to retry, since
-# we are handling retry logic ourselves.
 httplib2.RETRIES = 1
-
-# Maximum number of times to retry before giving up.
 MAX_RETRIES = 10
-
-# Always retry when these exceptions are raised.
 RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError, http.client.NotConnected,
                         http.client.IncompleteRead, http.client.ImproperConnectionState,
                         http.client.CannotSendRequest, http.client.CannotSendHeader,
                         http.client.ResponseNotReady, http.client.BadStatusLine)
-
-# Always retry when an apiclient.errors.HttpError with one of these status
-# codes is raised.
 RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
-
-# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
-# the OAuth 2.0 information for this application, including its client_id and
-# client_secret. You can acquire an OAuth 2.0 client ID and client secret from
-# the {{ Google Cloud Console }} at
-# {{ https://cloud.google.com/console }}.
-# Please ensure that you have enabled the YouTube Data API for your project.
-# For more information about using OAuth2 to access the YouTube Data API, see:
-#   https://developers.google.com/youtube/v3/guides/authentication
-# For more information about the client_secrets.json file format, see:
-#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 CLIENT_SECRETS_FILE = '../../credentials/client_secret.json'
 
-# This OAuth 2.0 access scope allows an application to upload files to the
-# authenticated user's YouTube channel, but doesn't allow other types of access.
 SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
 API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
 
 VALID_PRIVACY_STATUSES = ('public', 'private', 'unlisted')
+
+project_path = str(pathlib.Path(__file__).parent.parent.parent.absolute())
+today = datetime.date.today().strftime('%Y-%m-%d')
+today_fr = datetime.date.today().strftime('%d-%m-%Y')
+article_path = os.path.join(project_path, "data", today, "articles_filt.json")
+youtube_video_id_path = os.path.join(project_path, "data", today, "youtube_video_id.txt")
 
 
 # Authorize the request and store authorization credentials.
@@ -77,29 +58,15 @@ def initialize_upload(youtube, options):
         )
     )
 
-    # Call the API's videos.insert method to create and upload the video.
     insert_request = youtube.videos().insert(
         part=','.join(body.keys()),
         body=body,
-        # The chunksize parameter specifies the size of each chunk of data, in
-        # bytes, that will be uploaded at a time. Set a higher value for
-        # reliable connections as fewer chunks lead to faster uploads. Set a lower
-        # value for better recovery on less reliable connections.
-        #
-        # Setting 'chunksize' equal to -1 in the code below means that the entire
-        # file will be uploaded in a single HTTP request. (If the upload fails,
-        # it will still be retried where it left off.) This is usually a best
-        # practice, but if you're using Python older than 2.6 or if you're
-        # running on App Engine, you should set the chunksize to something like
-        # 1024 * 1024 (1 megabyte).
         media_body=MediaFileUpload(options["file"], chunksize=-1, resumable=True)
     )
 
     resumable_upload(insert_request)
 
 
-# This method implements an exponential backoff strategy to resume a
-# failed upload.
 def resumable_upload(request):
     response = None
     error = None
@@ -110,6 +77,9 @@ def resumable_upload(request):
             status, response = request.next_chunk()
             if response is not None:
                 if 'id' in response:
+                    f = open(youtube_video_id_path, "w")
+                    f.write(response['id'])
+                    f.close()
                     print('Video id "%s" was successfully uploaded.' % response['id'])
                 else:
                     exit('The upload failed with an unexpected response: %s' % response)
@@ -135,10 +105,7 @@ def resumable_upload(request):
 
 
 if __name__ == '__main__':
-    project_path = str(pathlib.Path(__file__).parent.parent.parent.absolute())
-    today = datetime.date.today().strftime('%Y-%m-%d')
-    today_fr = datetime.date.today().strftime('%d-%m-%Y')
-    article_path = os.path.join(project_path, "data", today, "articles_filt.json")
+
     articles = json.loads(open(article_path, "r"))
 
     articles_body = ""
