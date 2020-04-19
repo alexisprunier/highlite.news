dbfc99acbb13a21ccc90ae5bff0afeddd0b1f319
alexisprunier <prunier.al@gmail.com>
2020-04-19 12:15:38 +0200

    Adjust menu in webapp

--- webapp/src/component/Menu.jsx
+++ webapp/src/component/Menu.jsx
@@ -28,19 +28,29 @@ class Menu extends React.Component {
 					className="cn-button" 
 					id="cn-button"
 					onClick={this.onMenuClick}>
-					{this.state.open ? "Close": "Menu"}
+					{this.state.open ? <i class="fas fa-times"></i>: <i class="fas fa-bars"></i>}
 				</button>
 				<div 
 					className={"cn-wrapper " + (this.state.open ? "opened-nav": "")} 
 					id="cn-wrapper">
 					<ul>
-						<li><a href="#"><span>About</span></a></li>
-						<li><a href="#"><span>Tutorials</span></a></li>
-						<li><a href="#"><span>Articles</span></a></li>
-						<li><a href="#"><span>Snippets</span></a></li>
-						<li><a href="#"><span>Plugins</span></a></li>
-						<li><a href="#"><span>Contact</span></a></li>
-						<li><a href="#"><span>Follow</span></a></li>
+						<li><a href="https://www.youtube.com/channel/UCtqK3jsmIpnuIQT105cKr2A" target="_blank">
+							<span><i class="fab fa-youtube"></i></span></a>
+						</li>
+						<li><a href="https://twitter.com/highlitenews" target="_blank">
+							<span><i class="fab fa-twitter"></i></span></a>
+						</li>
+						<li><a href="https://www.instagram.com/highlite.news/?hl=en" target="_blank">
+							<span><i class="fab fa-instagram"></i></span></a>
+						</li>
+						<li><a href="#"><span><i class="fas fa-home"></i></span></a></li>
+						<li><a href="#"><span><i class="fas fa-person-booth"></i></span></a></li>
+						<li><a href="https://www.snapchat.com/add/highlite.news" target="_blank">
+							<span><i class="fab fa-snapchat-ghost"></i></span></a>
+						</li>
+						<li><a href="https://tiktok.com/@highlite.news" target="_blank">
+							<span><i class="fas fa-music"></i></span></a>
+						</li>
 					 </ul>
 				</div>
 			</div>
