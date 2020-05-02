d7e4bfb82067c2aa2955ce54adb5fd3f21c8d276
apruniergold <alexis@goldbaum.io>
2020-05-02 13:52:18 +0200

    Add the vote tag

--- webapp/src/component/Menu.jsx
+++ webapp/src/component/Menu.jsx
@@ -34,8 +34,14 @@ class Menu extends React.Component {
 					className={"cn-wrapper " + (this.state.open ? "opened-nav": "")} 
 					id="cn-wrapper">
 					<ul>
-						<li><a href="https://www.youtube.com/channel/UCtqK3jsmIpnuIQT105cKr2A" target="_blank">
-							<span><i className="fab fa-youtube" style={{"color": "#FF0000"}}></i></span></a>
+						<li><a href="#" onClick={() => this.props.changeMenu("HOME")}>
+							<span><i className="fas fa-home"></i></span></a>
+						</li>
+						<li><a href="#" onClick={() => this.props.changeMenu("VOTE")}>
+							<span><i className="fas fa-person-booth"></i></span></a>
+						</li>
+						<li><a href="https://www.facebook.com/highlite.news" target="_blank">
+							<span><i className="fab fa-facebook" style={{"color": "#3B5998"}}></i></span></a>
 						</li>
 						<li><a href="https://twitter.com/highlitenews" target="_blank">
 							<span><i className="fab fa-twitter" style={{"color": "#1DA1F2"}}></i></span></a>
@@ -43,17 +49,11 @@ class Menu extends React.Component {
 						<li><a href="https://www.instagram.com/highlite.news/?hl=en" target="_blank">
 							<span><i className="fab fa-instagram" style={{"color": "#8a3ab9"}}></i></span></a>
 						</li>
-						<li><a href="#" onClick={() => this.props.changeMenu("HOME")}>
-							<span><i className="fas fa-home"></i></span></a>
-						</li>
-						<li><a href="#" onClick={() => this.props.changeMenu("VOTE")}>
-							<span><i className="fas fa-person-booth"></i></span></a>
+						<li><a href="https://tiktok.com/@highlite.news" target="_blank">
+							<span><i className="fas fa-music" style={{"color": "#EE1D52"}}></i></span></a>
 						</li>
 						<li><a href="https://www.snapchat.com/add/highlite.news" target="_blank">
 							<span><i className="fab fa-snapchat-ghost" style={{"color": "#000000"}}></i></span></a>
-						</li>
-						<li><a href="https://tiktok.com/@highlite.news" target="_blank">
-							<span><i className="fas fa-music" style={{"color": "#EE1D52"}}></i></span></a>
 						</li>
 					 </ul>
 				</div>
