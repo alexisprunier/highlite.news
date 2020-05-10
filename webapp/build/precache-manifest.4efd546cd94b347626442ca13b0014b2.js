495b1dc4caec9a4289157fcef803f312792a900b
apruniergold <alexis@goldbaum.io>
2020-05-10 14:52:57 +0200

    Add build

---
+++ webapp/build/precache-manifest.4efd546cd94b347626442ca13b0014b2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d5a65ab045abdda3b3c8",
+    "url": "/static/js/main.d5a65ab0.chunk.js"
+  },
+  {
+    "revision": "bae13591f600c40a355c",
+    "url": "/static/js/1.bae13591.chunk.js"
+  },
+  {
+    "revision": "d5a65ab045abdda3b3c8",
+    "url": "/static/css/main.7a0927ad.chunk.css"
+  },
+  {
+    "revision": "bae13591f600c40a355c",
+    "url": "/static/css/1.e29c15ac.chunk.css"
+  },
+  {
+    "revision": "1bd71cfcba75635d2f07a3c6930ca83c",
+    "url": "/index.html"
+  }
+];
