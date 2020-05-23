f8ec4b1545ef2779d6fa40ff24a6693afe506a17
apruniergold <alexis@goldbaum.io>
2020-05-23 20:47:45 +0200

    Add exception wrapper

--- webserv/script/cron.py
+++ webserv/script/cron.py
@@ -36,50 +36,55 @@ class Cron:
 
     def run(self):
 
-        db = DB()
-        pipelines = db.get(db.tables["Pipeline"])
+        try:
 
-        @log_manager
-        def scrap(db, category):
-            try:
-                ScrapArticles.run(db, category)
-            except AlreadyGeneratedException as e:
-                print(e)
+            db = DB()
+            pipelines = db.get(db.tables["Pipeline"])
 
-        @log_manager
-        def generate(db, category):
-            try:
-                GenerateVideo.run(db, category, 'youtube')
-            except AlreadyGeneratedException as e:
-                print(e)
-            try:
-                GenerateVideo.run(db, category, 'instagram')
-            except AlreadyGeneratedException as e:
-                print(e)
-            try:
-                GenerateVideo.run(db, category, 'tiktok')
-            except AlreadyGeneratedException as e:
-                print(e)
+            @log_manager
+            def scrap(db, category):
+                try:
+                    ScrapArticles.run(db, category)
+                except AlreadyGeneratedException as e:
+                    print(e)
 
-        @log_manager
-        def upload(db, category):
-            try:
-                UploadYoutube.run(db, category)
-            except UploadException as e:
-                print(e)
-            time.sleep(30)
-            try:
-                UploadTwitter.run(db, category)
-            except UploadException as e:
-                print(e)
+            @log_manager
+            def generate(db, category):
+                try:
+                    GenerateVideo.run(db, category, 'youtube')
+                except AlreadyGeneratedException as e:
+                    print(e)
+                try:
+                    GenerateVideo.run(db, category, 'instagram')
+                except AlreadyGeneratedException as e:
+                    print(e)
+                try:
+                    GenerateVideo.run(db, category, 'tiktok')
+                except AlreadyGeneratedException as e:
+                    print(e)
 
-        for p in pipelines:
-            schedule.every().day.at(p.scrap_time).do(scrap, db, p.category)
-            schedule.every().day.at(p.generation_time).do(generate, db, p.category)
-            if p.publication_time is not None:
-                schedule.every().day.at(p.publication_time).do(upload, db, p.category)
+            @log_manager
+            def upload(db, category):
+                try:
+                    UploadYoutube.run(db, category)
+                except UploadException as e:
+                    print(e)
+                time.sleep(30)
+                try:
+                    UploadTwitter.run(db, category)
+                except UploadException as e:
+                    print(e)
 
-        while True:
-            print("check")
-            schedule.run_pending()
-            time.sleep(60)
+            for p in pipelines:
+                schedule.every().day.at(p.scrap_time).do(scrap, db, p.category)
+                schedule.every().day.at(p.generation_time).do(generate, db, p.category)
+                if p.publication_time is not None:
+                    schedule.every().day.at(p.publication_time).do(upload, db, p.category)
+
+            while True:
+                print("check")
+                schedule.run_pending()
+                time.sleep(60)
+
+        except Exception as e:
+            print("ERROR", e)
